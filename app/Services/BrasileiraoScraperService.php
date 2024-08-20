962fe33e86a7d4a0ae81b66a9a259b357af41183
kage3f <kage3f@gmail.com>
2024-08-19 21:38:37 -0300

    Feat: Implementar novos campos para a rota da tabela do brasileirão

--- app/Services/BrasileiraoScraperService.php
+++ app/Services/BrasileiraoScraperService.php
@@ -18,21 +18,45 @@ class BrasileiraoScraperService
             
             Log::info('Página acessada com sucesso');
 
-            $times = $crawler->filter('table.data-table.name tbody tr')->each(function (Crawler $node) {
-                $posicao = $node->filter('td.team .position')->text();
-                $nome = $node->filter('td.team .name div.visible-sm')->text();
-                $sigla = $node->filter('td.team .name div.visible-xs')->text();
+            $times = $crawler->filter('table.data-table.name tbody tr')->each(function (Crawler $node, $i) use ($crawler) {
+                $posicao = $node->filter('.position')->text();
+                $nome = $node->filter('.name div.visible-sm')->text();
+                $sigla = $node->filter('.name div.visible-xs')->text();
+                $brasao = $node->filter('.team-crest img')->attr('src');
+
+                // Buscando as estatísticas correspondentes na tabela de pontos
+                $statsNode = $crawler->filter('table.data-table.score tbody tr')->eq($i);
+                $pontos = $statsNode->filter('td')->eq(0)->text();
+                $jogos = $statsNode->filter('td')->eq(1)->text();
+                $vitorias = $statsNode->filter('td')->eq(2)->text();
+                $empates = $statsNode->filter('td')->eq(3)->text();
+                $derrotas = $statsNode->filter('td')->eq(4)->text();
+                $saldoGols = $statsNode->filter('td')->eq(7)->text();
 
                 Log::info("Dados extraídos para o time", [
                     'posicao' => $posicao,
                     'nome' => $nome,
-                    'sigla' => $sigla
+                    'sigla' => $sigla,
+                    'brasao' => $brasao,
+                    'pontos' => $pontos,
+                    'jogos' => $jogos,
+                    'vitorias' => $vitorias,
+                    'empates' => $empates,
+                    'derrotas' => $derrotas,
+                    'saldoGols' => $saldoGols
                 ]);
 
                 return [
                     'posicao' => trim($posicao),
                     'nome' => trim($nome),
                     'sigla' => trim($sigla),
+                    'brasao' => $brasao,
+                    'pontos' => (int)$pontos,
+                    'jogos' => (int)$jogos,
+                    'vitorias' => (int)$vitorias,
+                    'empates' => (int)$empates,
+                    'derrotas' => (int)$derrotas,
+                    'saldoGols' => (int)$saldoGols
                 ];
             });
 

e6c4cb9b3a8546b8f9b2c727759d1f868eafad74
kage3f <kage3f@gmail.com>
2024-08-21 22:39:15 -0300

    Feat: Implementar para vir por padrão a rodada atual do brasileirão

--- app/Services/BrasileiraoRodadasService.php
+++ app/Services/BrasileiraoRodadasService.php
@@ -16,16 +16,29 @@ class BrasileiraoRodadasService
         $this->client = new Client();
     }
 
-    public function getRodada($numero)
+    public function getRodada($numero = null)
     {
         try {
             $crawler = $this->client->request('GET', $this->url);
+            Log::info('URL acessada', ['url' => $this->url]);
             
+            $rodadaAtual = $this->getCurrentRodada($crawler);
+            
+            if ($numero === null) {
+                $numero = $rodadaAtual;
+                Log::info('Usando rodada mais recente encontrada', ['rodada' => $numero]);
+            } else {
+                $numero = (int) $numero;
+                Log::info('Usando rodada fornecida pelo usuário', ['rodada' => $numero]);
+            }
+    
             $rodadaDiv = $crawler->filter(".rodadas_grupo_A_numero_rodada_{$numero}")->first();
-
+    
             if ($rodadaDiv->count() === 0) {
+                Log::warning('Div da rodada não encontrada', ['rodada' => $numero]);
                 throw new \Exception("Rodada {$numero} não encontrada.");
             }
+    
 
             $jogos = $rodadaDiv->filter('.table__games__item')->each(function (Crawler $node) {
                 $date = $node->filter('.date')->text();
@@ -64,6 +77,30 @@ class BrasileiraoRodadasService
             ]);
             throw $e;
         }
+    }
+
+    private function getCurrentRodada(Crawler $crawler)
+    {
+        Log::info('Iniciando getCurrentRodada');
+        
+        $rodadaElement = $crawler->filter('[class*="rodadas_grupo_A_numero_rodada_"].mostrarRodada')->first();
+        
+        if ($rodadaElement->count() === 0) {
+            Log::warning('Elemento da rodada atual não encontrado');
+            throw new \Exception("Elemento da rodada atual não encontrado.");
+        }
+    
+        $classNames = $rodadaElement->attr('class');
+        preg_match('/rodadas_grupo_A_numero_rodada_(\d+)/', $classNames, $matches);
+        
+        if (!isset($matches[1])) {
+            Log::error('Não foi possível extrair o número da rodada', ['classes' => $classNames]);
+            throw new \Exception("Não foi possível determinar a rodada atual.");
+        }
+    
+        $rodadaAtual = (int) $matches[1];
+        Log::info('Rodada atual extraída com sucesso', ['rodada' => $rodadaAtual]);
+        return $rodadaAtual;
     }
 
     private function getTeamInfo(Crawler $node, $selector)
